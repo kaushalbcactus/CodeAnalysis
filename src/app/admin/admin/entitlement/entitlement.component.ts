8529d82d7443dc67763738f948781b3ad4acd5e8
kushwahaarvindb <arvind.kushwaha@cactusglobal.com>
2019-10-07 12:34:41 +0530

    Added code for group description visiblility
    Added code for group description visiblility

--- src/app/admin/admin/entitlement/entitlement.component.ts
+++ src/app/admin/admin/entitlement/entitlement.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SPOperationService } from 'src/app/Services/spoperation.service';
+import { GlobalService } from 'src/app/Services/global.service';
 
 @Component({
   selector: 'app-entitlement',
@@ -7,17 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntitlementComponent implements OnInit {
   public navLinks = [];
-  constructor() { }
+  constructor(
+    private spServices: SPOperationService,
+    private globalObject: GlobalService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.navLinks = [
       { routerLink: ['/admin/entitlement/userRoleMapping'], label: 'User to Role Mapping', value: 'UserToRoleMapping' },
       { routerLink: ['/admin/entitlement/roleUserMapping'], label: 'Role to User Mapping', value: 'RoleToUserMapping' },
       { routerLink: ['/admin/entitlement/copyPermission'], label: 'Copy Permission', value: 'Copy Permission' },
       { routerLink: ['/admin/entitlement/addUserToSow'], label: 'Add User To Sow', value: 'AddUserToSow' },
-      { routerLink: ['/admin/entitlement/addUserToProjects'], label: 'Add User To Projects', value: 'AddUserToProjects' },
-      { routerLink: ['/admin/entitlement/addGroupDescription'], label: 'Add Group Description', value: 'Add Group Description' }
+      { routerLink: ['/admin/entitlement/addUserToProjects'], label: 'Add User To Projects', value: 'AddUserToProjects' }
     ];
+    await this.checkUserPresentInGroup();
   }
-
+  async checkUserPresentInGroup() {
+    const isPresent = false;
+    const userInfo = await this.spServices.getUserInfo(this.globalObject.sharePointPageObject.userId);
+    if (userInfo && userInfo.hasOwnProperty('Groups')) {
+      if (userInfo.Groups && userInfo.Groups.results && userInfo.Groups.results.length) {
+        console.log(userInfo.Groups.results);
+        if (userInfo.Groups.results.some(x => x.Title === 'SPTeam')) {
+          this.navLinks.push({
+            routerLink: ['/admin/entitlement/addGroupDescription'],
+            label: 'Add Group Description', value: 'Add Group Description'
+          });
+        }
+      }
+    }
+  }
 }
