4ab7d3a737a72e6101019a2eaf06861878cfeb6f
ashishpCACTUS <49706779+ashishpCACTUS@users.noreply.github.com>
2019-11-06 20:57:09 +0530

    Fix for CA

--- src/app/ca/caservices/cacommon.service.ts
+++ src/app/ca/caservices/cacommon.service.ts
@@ -878,18 +878,18 @@ export class CACommonService {
         case 'QC':
         case 'Review-QC':
         case 'Inco-QC':
-          arrQualityCheckerIds.push(task.allocatedResource.ID);
+          arrQualityCheckerIds.push(task.allocatedResource.UserName.ID);
           break;
         case 'Edit':
         case 'Review-Edit':
         case 'Inco-Edit':
         case 'Galley':
-          arrEditorsIds.push(task.allocatedResource.ID);
+          arrEditorsIds.push(task.allocatedResource.UserName.ID);
           break;
         case 'Graphics':
         case 'Review-Graphics':
         case 'Inco-Graphics':
-          arrGraphicsIds.push(task.allocatedResource.ID);
+          arrGraphicsIds.push(task.allocatedResource.UserName.ID);
           break;
       }
     }
