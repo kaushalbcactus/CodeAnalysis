119b98057f4564c79335984ddec74508038dc799
maxwellfargoseCactus <49706940+maxwellfargoseCactus@users.noreply.github.com>
2020-04-03 20:02:53 +0530

    add new component file upload

---
+++ src/app/shared/file-upload-progress-dialog/file-upload-progress-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-upload-progress-dialog',
+  templateUrl: './file-upload-progress-dialog.component.html',
+  styleUrls: ['./file-upload-progress-dialog.component.css']
+})
+export class FileUploadProgressDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
