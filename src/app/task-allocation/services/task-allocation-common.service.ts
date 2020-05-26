0c902144c4cc38753344e94c1dcec98b4c5456dc
Ashish Padhy <ashish.padhy@cactusglobal.com>
2020-05-26 20:32:48 +0530

    on task drag open time picker in gantt

--- src/app/task-allocation/services/task-allocation-common.service.ts
+++ src/app/task-allocation/services/task-allocation-common.service.ts
@@ -398,4 +398,15 @@ export class TaskAllocationCommonService {
     //ganttObject.showAllocationSplit = data.AllocationPerDay ? true : false;
     return ganttObject;
   }
+
+  setMinutesAfterDrag(date) {
+    let time: any = this.getTimePart(date);
+    time = time.split(':')
+    let h = parseInt(time[0])
+    let m = parseInt(time[1].split(' ')[0])
+    let ampm = time[1].split(' ')[1]
+    let minutes = (Math.round(m / 15) * 15) % 60;
+    return h + ':' + minutes + ' ' + ampm;
+  }
+  
 }
