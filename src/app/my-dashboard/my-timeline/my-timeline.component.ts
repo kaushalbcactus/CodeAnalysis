6db95f19dcab9685dc62d788ad26f853d782dbf7
Ashish Padhy <ashish.padhy@cactusglobal.com>
2020-07-01 20:36:06 +0530

    my timeline issue fix

--- src/app/my-dashboard/my-timeline/my-timeline.component.ts
+++ src/app/my-dashboard/my-timeline/my-timeline.component.ts
@@ -683,98 +683,90 @@ export class MyTimelineComponent implements OnInit {
     const stval = await this.myDashboardConstantsService.getPrevTaskStatus(task);
     const allowedStatus = ["Completed", "Auto Closed"];
     // if (allowedStatus.includes(stval) || stval === '') {
+    
       this.commonService.confirmMessageDialog('Confirmation', 'Are you sure that you want to proceed?', null, ['Yes', 'No'], false).then(async Confirmation => {
-      if (Confirmation === 'Yes') {
-      if (task.Status === 'Completed' && !task.FinalDocSubmit && (allowedStatus.includes(stval) || stval === '')) {
-        this.commonService.showToastrMessage(this.constants.MessageType.error, 'No Final Document Found', false);
-        task.Status = earlierStaus;
-        this.CalendarLoader = false;
-        return false;
-      } else {
-        if (task.Status === "Completed" && (allowedStatus.includes(stval) || stval === '')) {
-          this.CalendarLoader = false;
-              task.parent = 'Dashboard';
-              const qmsTasks = await this.myDashboardConstantsService.callQMSPopup(task);
-              if (qmsTasks.length) {
-
-                const ref = this.dialogService.open(FeedbackPopupComponent, {
-                  data: {
-                    qmsTasks,
-                    task
-                  },
-                  header: 'Rate Work',
-                  width: '70vw',
-                  contentStyle: { 'min-height': '30vh', 'max-height': '90vh', 'overflow-y': 'auto' }
-                });
-                ref.onClose.subscribe((feedbacktask: any) => {
-                  if (feedbacktask) {
-                    this.saveTask(feedbacktask);
+        if (Confirmation === 'Yes') {
+          if (task.Status === 'Completed' && !task.FinalDocSubmit && (allowedStatus.includes(stval) || stval === '')) {
+            this.commonService.showToastrMessage(this.constants.MessageType.error, 'No Final Document Found', false);
+            task.Status = earlierStaus;
+            this.CalendarLoader = false;
+            return false;
+          } else {
+            if (task.Status === "Completed" && (allowedStatus.includes(stval) || stval === '')) {
+              this.CalendarLoader = false;
+                  task.parent = 'Dashboard';
+                  const qmsTasks = await this.myDashboardConstantsService.callQMSPopup(task);
+                  if (qmsTasks.length) {
+    
+                    const ref = this.dialogService.open(FeedbackPopupComponent, {
+                      data: {
+                        qmsTasks,
+                        task
+                      },
+                      header: 'Rate Work',
+                      width: '70vw',
+                      contentStyle: { 'min-height': '30vh', 'max-height': '90vh', 'overflow-y': 'auto' }
+                    });
+                    ref.onClose.subscribe((feedbacktask: any) => {
+                      if (feedbacktask) {
+                        this.saveTask(feedbacktask);
+                      }
+                    });
+                  } else {
+                    this.saveTask(task);
                   }
-                });
-              } else {
-                this.saveTask(task);
-              }
-            
-        } else if (task.Status === "In Progress" && (allowedStatus.includes(stval) || stval === '')) {
-          const batchURL = [];
-          if (this.task.StartTime) {
-            const startTime = this.commonService.ConvertTimeformat(24, this.task.StartTime);
-            this.task.StartDate = this.datePipe.transform(new Date(this.task.StartDate), 'yyyy-MM-dd' + 'T' + startTime + ':00.000');
+                
+            } else if (task.Status === "In Progress" && (allowedStatus.includes(stval) || stval === '')) {
+            const batchURL = [];
+            if (this.task.StartTime) {
+              const startTime = this.commonService.ConvertTimeformat(24, this.task.StartTime);
+              this.task.StartDate = this.datePipe.transform(new Date(this.task.StartDate), 'yyyy-MM-dd' + 'T' + startTime + ':00.000');
+            }
+            if (this.task.DueTime) {
+              const endTime = this.commonService.ConvertTimeformat(24, this.task.DueTime);
+              this.task.DueDate = this.datePipe.transform(this.task.DueDateDT, 'yyyy-MM-dd' + 'T' + endTime + ':00.000');
+            }
+    
+            const ProjectInformation = await this.myDashboardConstantsService.getCurrentTaskProjectInformation(task.ProjectCode);
+    
+            this.SelectedStatus = undefined;
+            this.taskdisplay = false;
+            this.CalendarLoader = true;
+            const jsonData = {
+              __metadata: { type: this.constants.listNames.Schedules.type },
+              Actual_x0020_Start_x0020_Date: task.Actual_x0020_Start_x0020_Date !== null ? task.Actual_x0020_Start_x0020_Date : new Date(),
+              Status: task.Status,
+              StartDate: this.task.StartDate,
+              DueDateDT: this.task.DueDate
+            };
+            this.commonService.SetNewrelic('MyDashboard', 'My-timeline', 'UpdateTask');
+            const taskUpdateUrl = this.spServices.getItemURL(this.constants.listNames.Schedules.name, task.ID);
+            this.commonService.setBatchObject(batchURL, taskUpdateUrl, jsonData, this.constants.Method.PATCH, this.constants.listNames.Schedules.name)
+    
+            const projectInfoUpdateurl = this.spServices.getItemURL(this.constants.listNames.ProjectInformation.name, ProjectInformation.ID);
+    
+    
+            this.commonService.setBatchObject(batchURL, projectInfoUpdateurl, { Status: this.constants.STATUS.IN_PROGRESS, __metadata: { type: this.constants.listNames.ProjectInformation.type } }, this.constants.Method.PATCH, this.constants.listNames.ProjectInformation.name)
+    
+            await this.spServices.executeBatch(batchURL);
+            if (task.ParentSlot) {
+              await this.myDashboardConstantsService.getCurrentAndParentTask(task, jsonData.Status);
+            }
+            this.commonService.showToastrMessage(this.constants.MessageType.success, ' task updated successfully.', false);
+            this.getEvents(false, this.fullCalendar.calendar.state.dateProfile.currentRange.start,
+              this.fullCalendar.calendar.state.dateProfile.currentRange.end);
+          } else {
+            this.commonService.showToastrMessage(this.constants.MessageType.warn, 'Previous task should be completed.', false);
+            task.Status = earlierStaus;
+            this.CalendarLoader = false;
           }
-          if (this.task.DueTime) {
-            const endTime = this.commonService.ConvertTimeformat(24, this.task.DueTime);
-            this.task.DueDate = this.datePipe.transform(this.task.DueDateDT, 'yyyy-MM-dd' + 'T' + endTime + ':00.000');
           }
-        });
-      } else if (task.Status === "In Progress" && (allowedStatus.includes(stval) || stval === '')) {
-        const batchURL = [];
-        if (this.task.StartTime) {
-          const startTime = this.commonService.ConvertTimeformat(24, this.task.StartTime);
-          this.task.StartDate = this.datePipe.transform(new Date(this.task.StartDate), 'yyyy-MM-dd' + 'T' + startTime + ':00.000');
+        } else {
+          task.Status = earlierStaus;
+          this.CalendarLoader = false;
         }
-        if (this.task.DueTime) {
-          const endTime = this.commonService.ConvertTimeformat(24, this.task.DueTime);
-          this.task.DueDate = this.datePipe.transform(this.task.DueDateDT, 'yyyy-MM-dd' + 'T' + endTime + ':00.000');
-        }
-
-        const ProjectInformation = await this.myDashboardConstantsService.getCurrentTaskProjectInformation(task.ProjectCode);
-
-        this.SelectedStatus = undefined;
-        this.taskdisplay = false;
-        this.CalendarLoader = true;
-        const jsonData = {
-          __metadata: { type: this.constants.listNames.Schedules.type },
-          Actual_x0020_Start_x0020_Date: task.Actual_x0020_Start_x0020_Date !== null ? task.Actual_x0020_Start_x0020_Date : new Date(),
-          Status: task.Status,
-          StartDate: this.task.StartDate,
-          DueDateDT: this.task.DueDate
-        };
-        this.commonService.SetNewrelic('MyDashboard', 'My-timeline', 'UpdateTask');
-        const taskUpdateUrl = this.spServices.getItemURL(this.constants.listNames.Schedules.name, task.ID);
-        this.commonService.setBatchObject(batchURL, taskUpdateUrl, jsonData, this.constants.Method.PATCH, this.constants.listNames.Schedules.name)
-
-        const projectInfoUpdateurl = this.spServices.getItemURL(this.constants.listNames.ProjectInformation.name, ProjectInformation.ID);
-
-
-        this.commonService.setBatchObject(batchURL, projectInfoUpdateurl, { Status: this.constants.STATUS.IN_PROGRESS, __metadata: { type: this.constants.listNames.ProjectInformation.type } }, this.constants.Method.PATCH, this.constants.listNames.ProjectInformation.name)
-
-        await this.spServices.executeBatch(batchURL);
-        if (task.ParentSlot) {
-          await this.myDashboardConstantsService.getCurrentAndParentTask(task, jsonData.Status);
-        }
-        this.commonService.showToastrMessage(this.constants.MessageType.success, ' task updated successfully.', false);
-        this.getEvents(false, this.fullCalendar.calendar.state.dateProfile.currentRange.start,
-          this.fullCalendar.calendar.state.dateProfile.currentRange.end);
-      } else {
-        this.commonService.showToastrMessage(this.constants.MessageType.warn, 'Previous task should be completed.', false);
-        task.Status = earlierStaus;
-        this.CalendarLoader = false;
-      }
-    } else {
-      task.Status = earlierStaus;
-      this.CalendarLoader = false;
-    }
-    });
+      });
+    
   }
 
   async saveTask(task) {
