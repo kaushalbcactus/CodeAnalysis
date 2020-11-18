0bd87dcf4b5430628bbc477c3f4101c249cc1d86
maxwellfargoseCactus <49706940+maxwellfargoseCactus@users.noreply.github.com>
2020-11-18 15:36:36 +0530

    minor change

--- src/app/projectmanagement/projectmanagement/add-projects/manage-finance/manage-finance.component.ts
+++ src/app/projectmanagement/projectmanagement/add-projects/manage-finance/manage-finance.component.ts
@@ -2887,7 +2887,7 @@ export class ManageFinanceComponent implements OnInit {
     });
       
     } else {
-      this.commonService.showToastrMessage(this.constant.MessageType.warn, "Unable to move to new PO, PO doesn't exist.",false);
+      this.commonService.showToastrMessage(this.constant.MessageType.warn, "No Scheduled Line Items Found to move.",false);
       return false;
     }
 
