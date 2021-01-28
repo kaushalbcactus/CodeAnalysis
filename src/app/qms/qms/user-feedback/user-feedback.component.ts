9649361cafe36a7bddad3198758e91e5291bdeed
kushwahaarvindb <arvind.kushwaha@cactusglobal.com>
2021-01-28 11:09:33 +0530

    chec

--- src/app/qms/qms/user-feedback/user-feedback.component.ts
+++ src/app/qms/qms/user-feedback/user-feedback.component.ts
@@ -56,7 +56,7 @@ export class UserFeedbackComponent implements OnInit, AfterViewChecked {
       { field: 'Date', header: 'Date', visibility: true, exportable: true, Type: 'datetime', dbName: 'Created', options: []  },
       { field: 'Task', header: 'Task', visibility: true, exportable: true, Type: 'string', dbName: 'SubMilestones', options: [] },
       { field: 'Type', header: 'Type', visibility: true, exportable: true, Type: 'string', dbName: 'FeedbackType', options: [] },
-      { field: 'Feedbackby', header: 'Feedback By', visibility: true, exportable: true, Type: 'string', dbName: 'Author.Title', options: [] },
+      { field: 'Feedbackby', header: 'Feedback By', visibility: true, exportable: true, Type: 'string', dbName: 'Feedbackby', options: [] },
       { field: 'Rating', header: 'Rating', visibility: true, exportable: true, Type: 'string', dbName: 'AverageRating', options: [] },
       { field: 'EvaluatorSkill', header: 'Evaluator Skill', visibility: true, exportable: true, Type: 'EvaluatorSkill', dbName: 'resource', options: [] },
       { field: 'Comments', header: 'Comments', visibility: true, exportable: true, Type: 'string', dbName: 'CommentsMT', options: [] },
@@ -64,7 +64,6 @@ export class UserFeedbackComponent implements OnInit, AfterViewChecked {
       { field: 'Score', header: 'Score', visibility: false, exportable: true, Type: 'string', dbName: 'resource', options: [] }
     ];
   }
-
   // colFilters(colData) {
   //   // tslint:disable: max-line-length
   //   this.UFColArray.Date = this.qmsCommon.uniqueArrayObj(colData.map(a => {
